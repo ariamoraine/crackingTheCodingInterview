0698d4a0ed4233f2d3a3dbe1b17a679349a64232
Aria Moraine <ariamoraine@gmail.com>
2017-06-28 12:57:38 -0400

    removed comments

--- stacksAndQueues/stack_findingMin.js
+++ stacksAndQueues/stack_findingMin.js
@@ -1,41 +1,41 @@
 class Stack {
 
   constructor () {
-    this.top = null;
-    this.min = [];
+    this.top = null
+    this.min = []
   }
 
   myPop () {
     if (this.top) {
-      const nodeToReturn = this.top;
-      this.top = this.top.next;
-      if(nodeToReturn.value == this.findMin()) {
+      const nodeToReturn = this.top
+      this.top = this.top.next
+      if (nodeToReturn.value == this.findMin()) {
         this.min.pop()
       }
-      return nodeToReturn;
+      return nodeToReturn
     }
     return false
   }
 
   peek () {
-    return this.top.value;
+    return this.top.value
   }
 
   myPush (value) {
-    const newNode = new Node(value)
+    const newNode = new Node (value)
     newNode.next = this.top
     this.top = newNode
-    if(this.min.length === 0) {
+    if (this.min.length === 0) {
       this.min.push(value)
     } else {
-      if(value <= this.min[this.min.length -1]) {
+      if (value <= this.min[this.min.length -1]) {
         this.min.push(value)
       }
     }
   }
 
   findMin () {
-    return this.min[this.min.length-1];
+    return this.min[this.min.length -1]
   }
 
 }
@@ -46,19 +46,3 @@ class Node {
     this.next = null
   }
 }
-
-const myStack = new Stack()
-console.log("BEFORE ADDING NEW NODE 2", myStack)
-
-myStack.myPush(5)
-console.log("AFTER FIRST PUSH", myStack)
-myStack.myPush(4)
-
-myStack.myPush(4)
-console.log(myStack)
-myStack.myPop()
-console.log(myStack)
-
-// console.log("AFTER LAST PUSH", myStack)
-// console.log("MIN", myStack.findMin())
-// console.log("PEEK", myStack.peek())
