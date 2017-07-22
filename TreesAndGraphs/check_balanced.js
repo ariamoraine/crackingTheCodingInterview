aeff955ee8992d2a0c1eaefc533fa012acd69b38
Aria Moraine <ariamoraine@gmail.com>
2017-07-22 11:30:46 -0400

    refactored function to take out the use of arrays

--- TreesAndGraphs/check_balanced.js
+++ TreesAndGraphs/check_balanced.js
@@ -24,24 +24,24 @@ Check Balanced: Implement a function to check if a binary tree is balance. For t
 
 function checkBalanced (tree, currDepth = 0) {
   currDepth++ //everytime we add one to the depth
-  if (!tree.left && !tree.right) return [currDepth] //if we got down to a leaf than just return the current depth
-  let leftResults = [], rightResults = [] //we start the left and right results so we never run into the edgecase of them being undefined
+  if (!tree.left && !tree.right) return currDepth //if we got down to a leaf than just return the current depth
+  let leftResults, rightResults //we start the left and right results so we never run into the edgecase of them being undefined
 
   if (tree.left) { //if there is a left side of the tree we need to recurse down that side
-    leftResults = leftResults.concat(checkBalanced(tree.left, currDepth)) //adding the results of the recursive call to our results
-    if (!tree.right) rightResults = rightResults.concat([currDepth]) //if the current tree does not have a right side then we will want to compare the current depth to whatever our recursive call to the left returns
+    leftResults = checkBalanced(tree.left, currDepth) //adding the results of the recursive call to our results
+    if (!tree.right) rightResults = currDepth //if the current tree does not have a right side then we will want to compare the current depth to whatever our recursive call to the left returns
   }
 
   //same logic as above but for the right side of the tree.
   if (tree.right){
-    rightResults = rightResults.concat(checkBalanced(tree.right, currDepth))
-    if (!tree.left) leftResults = leftResults.concat([currDepth])
+    rightResults = checkBalanced(tree.right, currDepth)
+    if (!tree.left) leftResults = currDepth
   }
 
-  if (!leftResults[0] || !rightResults[0]) return false //if either the left or the right sides of the tree has returned us false than just return false
-  if (Math.abs(leftResults[0] - rightResults[0]) > 1) return false //if the difference between the depths from the left side and the right side are greater than 1 that means we have found an imbalance and we will just return false
+  if (!leftResults || !rightResults) return false //if either the left or the right sides of the tree has returned us false than just return false
+  if (Math.abs(leftResults - rightResults) > 1) return false //if the difference between the depths from the left side and the right side are greater than 1 that means we have found an imbalance and we will just return false
 
-  const largerDepth = leftResults[0] > rightResults[0] ? leftResults : rightResults //we find the longer of the two depths and return that so that we can use that to compare to our previous call
+  const largerDepth = leftResults > rightResults ? leftResults : rightResults //we find the longer of the two depths and return that so that we can use that to compare to our previous call
 
   if (currDepth === 1) return true //if we are at the root of the tree than we don't want to return a number, we want to return true
   else return largerDepth //if we aren't at the root than we need to return the depth from the previous call to the parent so that we can use it to other side of the tree
