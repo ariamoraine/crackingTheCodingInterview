de2d7e24e6d8ed5d00c7373882ac2d3afb5b695f
Christine <cleverett1@gmail.com>
2017-08-15 10:13:21 -0400

    check subtree complete

---
+++ treesAndGraphs/check_subtree.js
@@ -0,0 +1,35 @@
+function checkSubtree(t1, t2){
+  let queue=[t1]
+  while(queue.length){
+    let curr = queue.shift()
+    if(curr.value === t2.value){
+      if(depthCheck(curr, t2)) return true
+      if(curr.left) queue.push(curr.left)
+      if(curr.right) queue.push(curr.right)
+    }
+  }
+  return false
+}
+
+function depthCheck(t1, t2){
+  if(t1.left && t2.left) return depthCheck(t1.left, t2.left)
+  if(t1.right && t2.right) return depthCheck(t1.right, t2.right)
+  if(!t1.left && !t2.left && t1 === t2) return true
+  return false
+}
+
+
+let subTree = {
+    value: 4,
+    left: { value: 3},
+    right: { value: 5}}
+
+let tree1 = {
+    value: 10,
+    left: subTree,
+    right:{ value: 19,
+       left: { value: 17 },
+       right: { value: 20 } } };
+
+
+console.log(depthCheck(tree1.left, subTree))
