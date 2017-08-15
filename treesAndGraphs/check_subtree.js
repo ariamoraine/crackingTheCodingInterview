df6917aad22e973e367f71309912edc444865e5e
Christine <cleverett1@gmail.com>
2017-08-15 10:27:07 -0400

    this actually works now

--- treesAndGraphs/check_subtree.js
+++ treesAndGraphs/check_subtree.js
@@ -4,9 +4,9 @@ function checkSubtree(t1, t2){
     let curr = queue.shift()
     if(curr.value === t2.value){
       if(depthCheck(curr, t2)) return true
-      if(curr.left) queue.push(curr.left)
-      if(curr.right) queue.push(curr.right)
     }
+    if(curr.left) queue.push(curr.left)
+    if(curr.right) queue.push(curr.right)
   }
   return false
 }
@@ -14,7 +14,7 @@ function checkSubtree(t1, t2){
 function depthCheck(t1, t2){
   if(t1.left && t2.left) return depthCheck(t1.left, t2.left)
   if(t1.right && t2.right) return depthCheck(t1.right, t2.right)
-  if(!t1.left && !t2.left && t1 === t2) return true
+  if(!t1.left && !t2.left && !t1.right && !t2.right && t1=== t2) return true
   return false
 }
 
@@ -26,10 +26,13 @@ let subTree = {
 
 let tree1 = {
     value: 10,
-    left: subTree,
+    left: {
+      value: 4,
+      left: { value: 3},
+      right: { value: 5}},
     right:{ value: 19,
        left: { value: 17 },
        right: { value: 20 } } };
 
 
-console.log(depthCheck(tree1.left, subTree))
+console.log(checkSubtree(tree1, subTree))
